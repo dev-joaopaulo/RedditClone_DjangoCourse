afa44ac7a34af47a9c345d28530cb03940610505
joaopspp <joaopspp@gmail.com>
2020-10-12 21:39:57 -0300

    class 06

--- redditclone/posts/views.py
+++ redditclone/posts/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from . import models
 
 # Create your views here.
 @login_required
 def create(request):
-    return render(request, 'posts/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['url']:
+            post = models.Post()
+            post.title = request.POST['title']
+            post.url = request.POST['url']
+            post.pub_date = timezone.datetime.now()
+            post.author = request.user
+            post.save()
+            return redirect('home')
+        else:
+            return render(request, 'posts/create.html', {'error': 'Please, complete all fields to submit your post'})
+    else:
+        return render(request, 'posts/create.html')
+
+
+def home(request):
+    posts = models.Post.objects.order_by("votes_total")
+    return render(request, 'posts/home.html', {'posts':posts})
